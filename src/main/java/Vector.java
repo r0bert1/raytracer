9ee1c120d50cda720166b8ab746b004caeca4434
r0bert1 <rpakkane@gmail.com>
2020-01-24 00:42:16 +0200

    updated methods to make the vector class immutable

--- src/main/java/Vector.java
+++ src/main/java/Vector.java
@@ -1,11 +1,36 @@
 public class Vector {
-    int x;
-    int y;
-    int z;
+    double x;
+    double y;
+    double z;
 
-    public Vector(int x, int y, int z) {
+    public Vector(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public double magnitude() {
+        return Math.sqrt(this.dot(this));
+    }
+
+    public Vector add(Vector v) {
+        return new Vector(x + v.x, y + v.y, z + v.z);
+    }
+
+    public Vector multiplyBy(double k) {
+        return new Vector(x * k, y * k, z * k);
+    }
+
+    public double dot(Vector v) {
+        return (x * v.x) + (y * v.y) + (z * v.z);
+    }
+
+    public Vector normalize() {
+        return multiplyBy(1 / magnitude());
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
     }
 }
