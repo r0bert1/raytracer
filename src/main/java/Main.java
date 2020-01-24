01e2cf7f54c83a0ea9bf23ae19662c6741d98800
r0bert1 <rpakkane@gmail.com>
2020-01-24 18:33:43 +0200

    now rendering red circle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,20 @@
 import java.io.*;
 
 public class Main {
-    public static Vector colorOf(Ray r) {
-        Vector unitVector = r.direction().normalize();
+    public static Boolean intersectsSphere(Vector center, double radius, Ray ray) {
+        Vector offsetOrigin = ray.origin().subtract(center);
+        double a = ray.direction().dot(ray.direction());
+        double b = 2.0 * offsetOrigin.dot(ray.direction());
+        double c = offsetOrigin.dot(offsetOrigin) - (radius * radius);
+        double discriminant = b * b - 4 * a * c;
+        return discriminant > 0;
+    }
+
+    public static Vector colorOf(Ray ray) {
+        if (intersectsSphere(new Vector(0, 0, -1), 0.5, ray)) {
+            return new Vector(1, 0, 0);
+        }
+        Vector unitVector = ray.direction().normalize();
         double t = 0.5 * (unitVector.y + 1.0);
         return new Vector(1.0, 1.0, 1.0).multiplyBy(1.0 - t)
                 .add(new Vector(0.5, 0.7, 1.0).multiplyBy(t));
@@ -23,8 +35,10 @@ public class Main {
 
             for (int y = rows - 1; y >= 0; y--) {
                 for (int x = 0; x < cols; x++) {
-                    double r = (double) x / (double) cols;
-                    double g = (double) y / (double) rows;
+                    double r = x / (double) cols;
+                    double g = y / (double) rows;
+                    // The ray's direction vector's endpoint starts at the top left corner
+                    // and is moved from left to right on every row of pixels on the screen.
                     Ray ray = new Ray(origin, lowerLeftCorner.add(
                             (horizontal.multiplyBy(r).add(
                                     vertical.multiplyBy(g)))));
