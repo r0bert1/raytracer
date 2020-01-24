d35cf7aa91f5adf9deede9fb7f5c930064e594b8
r0bert1 <rpakkane@gmail.com>
2020-01-24 22:42:54 +0200

    added javadocs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,20 @@ public class Main {
     final static int width = 200;
     final static int height = 100;
 
+    /**
+     * Checks if ray (equation: p(t) = a + t*b) intersects the specified
+     * sphere (equation: (p-c)*(p-c) = r*r) by inserting the ray expression in
+     * the sphere equation, which eventually yields
+     *      t*t*(b*b) + 2*t*(b*b-c) + (a-c)*(a-c) - r*r = 0
+     * where b is the direction vector of the ray, a is the position vector of
+     * the ray's source, C is the center of the circle and R is the radius of
+     * the sphere.
+     *
+     * @param center Position of sphere center
+     * @param radius Radius of sphere
+     * @param ray    The examined ray
+     * @return       true if ray intersects sphere, otherwise false
+     */
     public static Boolean intersectsSphere(Vector center, double radius, Ray ray) {
         Vector offsetOrigin = ray.source.subtract(center);
         double a = ray.direction.dot(ray.direction);
@@ -13,6 +27,15 @@ public class Main {
         return discriminant > 0;
     }
 
+    /**
+     * Determines the color seen along the provided ray. The color will be set
+     * to red if the ray intersects the sphere, and otherwise the color is set
+     * to a shade of blue depending on it's height on the screen to create a
+     * fading blue background.
+     *
+     * @param ray The ray whose color will be determined
+     * @return    A 'color vector' where vector.x = red, vector.y = green etc.
+     */
     public static Vector colorOf(Ray ray) {
         if (intersectsSphere(new Vector(0, 0, -1), 0.5, ray)) {
             return new Vector(1, 0, 0);
@@ -23,9 +46,17 @@ public class Main {
                 .add(new Vector(0.5, 0.7, 1.0).multiplyBy(t));
     }
 
+    /**
+     * Calculates the direction vector of a ray so that it hits the spot on
+     * the screen specified by the x and y arguments.
+     * The ray is initially directed towards the top left corner of the screen
+     * and it's moved around by scaling the offset vectors.
+     *
+     * @param x Specifies the target x coordinate on the screen
+     * @param y Specifies the target y coordinate on the screen
+     * @return  Direction vector of ray
+     */
     public static Vector rayDirection(int x, int y) {
-        // The ray is initially directed towards the top left corner of the screen
-        // and it's moved around by scaling the offset vectors.
         Vector lowerLeftCorner = new Vector(-2.0, -1.0, -1.0);
         Vector horizontalOffset = new Vector(4.0, 0.0, 0.0);
         Vector verticalOffset = new Vector(0.0, 2.0, 0.0);
@@ -40,6 +71,11 @@ public class Main {
                                         verticalOffset.multiplyBy(vScale)));
     }
 
+    /**
+     * Writes the image data in plain text to a .ppm file.
+     *
+     * @param fileName The name of the output file
+     */
     public static void writeImageToFile(String fileName) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
@@ -49,6 +85,7 @@ public class Main {
                 for (int x = 0; x < width; x++) {
                     Ray ray = new Ray(new Vector(0, 0, 0), rayDirection(x, y));
                     Vector colorVector = colorOf(ray);
+                    // RGB values are scaled to match the range of the .ppm file
                     int ir = (int) (255.99 * colorVector.x);
                     int ig = (int) (255.99 * colorVector.y);
                     int ib = (int) (255.99 * colorVector.z);
