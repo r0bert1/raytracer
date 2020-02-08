8e781cc7b1032e1bc3f220e17677ea1e614a9637
r0bert1 <rpakkane@gmail.com>
2020-02-08 15:58:39 +0200

    added alternative constructor and equals method

--- src/main/java/Hit.java
+++ src/main/java/Hit.java
@@ -4,4 +4,24 @@ public class Hit {
     Vector normal;
 
     public Hit() {}
+
+    public Hit(Vector position, Vector normal) {
+        this.position = position;
+        this.normal = normal;
+    }
+
+    @Override
+    public String toString() {
+        return "p: " + position + ", n: " + normal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hit hit = (Hit) o;
+        return Double.compare(hit.t, t) == 0 &&
+                position.equals(hit.position) &&
+                normal.equals(hit.normal);
+    }
 }
