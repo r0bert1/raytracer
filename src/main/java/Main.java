3cd23dadc00f45e4e3f202cffab1ad418d03895e
r0bert1 <rpakkane@gmail.com>
2020-02-13 19:37:55 +0200

    added random reflection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,9 +18,10 @@ public class Main {
      * @return    A 'color vector' where vector.x = red, vector.y = green etc.
      */
     public static Vector colorOf(Ray ray, Sphere[] spheres) {
-        Hit hit = hit(spheres, ray, 0.0, Double.MAX_VALUE);
+        Hit hit = hit(spheres, ray, 0.001, Double.MAX_VALUE);
         if (hit != null) {
-            return new Vector(hit.normal.x + 1, hit.normal.y + 1, hit.normal.z + 1).multiplyBy(0.5);
+            Vector target = hit.position.add(hit.normal).add(randomPointFromUnitSphere());
+            return colorOf(new Ray(hit.position, target.subtract(hit.position)), spheres).multiplyBy(0.5);
         } else {
             Vector unitVector = ray.direction.normalize();
             double t = 0.5 * (unitVector.y + 1.0);
@@ -51,6 +52,16 @@ public class Main {
         return hit;
     }
 
+    public static Vector randomPointFromUnitSphere() {
+        Vector point;
+        do {
+            point = new Vector(random.nextDouble(), random.nextDouble(), random.nextDouble())
+                    .multiplyBy(2)
+                    .subtract(new Vector(1, 1, 1));
+        } while (point.dot(point) >= 1);
+        return point;
+    }
+
     /**
      * Writes the image data to a file in plain ppm format.
      *
@@ -69,19 +80,20 @@ public class Main {
 
             for (int y = HEIGHT - 1; y >= 0; y--) {
                 for (int x = 0; x < WIDTH; x++) {
-                    Vector colorVector = new Vector(0, 0, 0);
+                    Vector color = new Vector(0, 0, 0);
                     for (int s = 0; s < SAMPLES; s++) {
                         double hScale = (x + random.nextDouble()) / (double) WIDTH;
                         double vScale = (y + random.nextDouble()) / (double) HEIGHT;
                         Ray ray = camera.getRay(hScale, vScale);
-                        colorVector = colorVector.add(colorOf(ray, spheres));
+                        color = color.add(colorOf(ray, spheres));
                     }
-                    colorVector = colorVector.multiplyBy(1 / (double) SAMPLES);
+                    color = color.multiplyBy(1 / (double) SAMPLES);
+                    color = new Vector(Math.sqrt(color.x), Math.sqrt(color.y), Math.sqrt(color.z));
 
                     // RGB values are scaled to match the range of the .ppm file
-                    int ir = (int) (255.99 * colorVector.x);
-                    int ig = (int) (255.99 * colorVector.y);
-                    int ib = (int) (255.99 * colorVector.z);
+                    int ir = (int) (255.99 * color.x);
+                    int ig = (int) (255.99 * color.y);
+                    int ib = (int) (255.99 * color.z);
                     writer.write(ir + " " + ig + " " + ib + "\n");
                 }
             }
